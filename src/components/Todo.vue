a477123b15c4e5017c979f356ba71d2ba1830e5d
rocksinghajay <rocksinghajay@gmail.com>
2019-02-02 04:05:23 +0530

    initial commit

---
+++ src/components/Todo.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="todo-container">
+    <h1>{{ msg }}</h1>
+    <div>
+      <input v-model="message" placeholder="Add Todo">
+      <p>Message is: {{ message }}</p>
+      <button v-on:click="addTodo">Add</button>
+    </div>
+      <ul>
+      <li v-for="(item, index) in todos">
+        Messsage {{item}}
+      </li>
+    </ul>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Todo',
+  props: {
+    msg: String
+  },
+  data () {
+    return {
+      message: '',
+      todos:[]
+    }
+  },
+  methods: {
+    addTodo() {
+      this.todos.push(this.message)
+      console.log(this.todos, 'dkdkk')
+    },
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
